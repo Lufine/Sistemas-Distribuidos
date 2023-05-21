176a15067f21444f795fa047ae5bb0e41f0b478f
Lufine <lfneuwirth@gmail.com>
2023-05-21 17:41:36 -0300

    Projeto Integrador Ciclo 2

--- Semana 13/Atividade Pratica/Cliente.py
+++ Semana 13/Atividade Pratica/Cliente.py
@@ -1,33 +1,111 @@
+#Curso de Engenharia de Software - UniEVANGÉLICA 
+#Disciplina de Sistemas Distribuidos 
+#Aluno: Luiz Filipe Neuwirth
+#DATA: 21/05/2023 
+
 import socket
+import sqlite3
 
-def start_client():
-    host = '127.0.0.1'  # Endereço IP do servidor
-    port = 12345  # Porta para conexão
+HOST = '127.0.0.1'  # Endereço IP do servidor
+PORT = 65432        # Porta do servidor
 
-    # Cria um socket TCP/IP
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Definir o protocolo de comunicação
+# O protocolo consiste em mensagens no formato "tipo|dados"
+# O tipo pode ser "MSG" para mensagens de texto ou "ITEM" para informações de item
+# Os dados podem ser uma mensagem de texto ou informações de item no formato "nome|quantidade"
+def receive_message(conn):
+    data = conn.recv(1024).decode()
+    parts = data.split('|')
+    return parts[0], parts[1]
 
-    # Conecta ao servidor
-    client_socket.connect((host, port))
+def send_message(conn, message):
+    data = f"MSG|{message}".encode()
+    conn.sendall(data)
 
-    while True:
-        # Solicita ao usuário para digitar uma mensagem
-        message = input('Digite uma solicitação para enviar ao servidor (ou "sair" para encerrar): ')
+def add_item_to_database(conn, item_name, item_quantity):
+    # Conectar ao banco de dados
+    conn = sqlite3.connect('nadespensa.db')
+    c = conn.cursor()
+    # Inserir o item na tabela de itens
+    c.execute("INSERT INTO itens (nome, quantidade) VALUES (?, ?)", (item_name, item_quantity))
+    conn.commit()
+    conn.close()
 
-        if message.lower() == 'sair':
-            break
+def remove_item_from_database(conn, item_name):
+    # Conectar ao banco de dados
+    conn = sqlite3.connect('nadespensa.db')
+    c = conn.cursor()
+    # Remover o item da tabela de itens
+    c.execute("DELETE FROM itens WHERE nome = ?", (item_name,))
+    conn.commit()
+    conn.close()
 
-        # Envia a solicitação para o servidor
-        request_message = 'REQ ' + message
-        client_socket.send(request_message.encode())
+def update_item_in_database(conn, item_name, item_quantity):
+    # Conectar ao banco de dados
+    conn = sqlite3.connect('nadespensa.db')
+    c = conn.cursor()
+    # Atualizar a quantidade do item na tabela de itens
+    c.execute("UPDATE itens SET quantidade = ? WHERE nome = ?", (item_quantity, item_name))
+    conn.commit()
+    conn.close()
 
-        # Recebe a resposta do servidor
-        response = client_socket.recv(1024).decode()
+def get_item_from_database(conn, item_name):
+    # Conectar ao banco de dados
+    conn = sqlite3.connect('nadespensa.db')
+    c = conn.cursor()
+    # Buscar o item na tabela de itens
+    c.execute("SELECT * FROM itens WHERE nome = ?", (item_name,))
+    item = c.fetchone()
+    conn.close()
+    return item
 
-        print('Resposta recebida do servidor:', response)
-
-    # Fecha a conexão com o servidor
-    client_socket.close()
-
-if __name__ == '__main__':
-    start_client()
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    print('Servidor aguardando conexões...')
+    conn, addr = s.accept()
+    with conn:
+        print('Conectado por', addr)
+        while True:
+            # Receber a mensagem do cliente
+            type, data = receive_message(conn)
+            if type == 'MSG':
+                print(f'Mensagem recebida: {data}')
+                # Enviar uma mensagem de resposta
+                send_message(conn, f'Recebido: {data}')
+            elif type == 'ADD':
+                print(f'Informações de item recebidas: {data}')
+                # Adicionar o item ao banco de dados
+                item_name, item_quantity = data.split('|')
+                add_item_to_database(conn, item_name, item_quantity)
+                # Enviar uma mensagem de resposta
+                send_message(conn, f'Item adicionado: {item_name} ({item_quantity})')
+            elif type == 'REMOVE':
+                print(f'Informações de item recebidas: {data}')
+                # Remover o item do banco de dados
+                item_name = data
+                remove_item_from_database(conn, item_name)
+                # Enviar uma mensagem de resposta
+                send_message(conn, f'Item removido: {item_name}')
+            elif type == 'UPDATE':
+                print(f'Informações de item recebidas: {data}')
+                # Atualizar a quantidade do item no banco de dados
+                item_name, item_quantity = data.split('|')
+                update_item_in_database(conn, item_name, item_quantity)
+                # Enviar uma mensagem de resposta
+                send_message(conn, f'Item atualizado: {item_name} ({item_quantity})')
+            elif type == 'GET':
+                print(f'Solicitação de informações de item recebida: {data}')
+                # Buscar o item no banco de dados
+                item_name = data
+                item = get_item_from_database(conn, item_name)
+                if item:
+                    # Enviar uma mensagem de resposta com as informações do item
+                    send_message(conn, f'Item encontrado: {item[0]} ({item[1]})')
+                else:
+                    # Enviar uma mensagem de resposta informando que o item não foi encontrado
+                    send_message(conn, f'Item não encontrado: {item_name}')
+            else:
+                print(f'Tipo de mensagem inválido: {type}')
+                # Enviar uma mensagem de erro
+                send_message(conn, f'Erro: tipo de mensagem inválido')
--- Semana 13/Atividade Pratica/Servidor.py
+++ Semana 13/Atividade Pratica/Servidor.py
@@ -1,51 +1,57 @@
+#Curso de Engenharia de Software - UniEVANGÉLICA 
+#Disciplina de Sistemas Distribuidos 
+#Aluno: Luiz Filipe Neuwirth
+#DATA: 21/05/2023 
+
 import socket
 
-def start_server():
-    host = '127.0.0.1'  # Endereço IP do servidor
-    port = 12345  # Porta para conexão
+HOST = '127.0.0.1'  # Endereço IP do servidor
+PORT = 65432        # Porta do servidor
 
-    # Cria um socket TCP/IP
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Define o protocolo de comunicação
+# O protocolo consiste em mensagens no formato "tipo|dados"
+# O tipo pode ser "MSG" para mensagens de texto ou "ITEM" para informações de item
+# Os dados podem ser uma mensagem de texto ou informações de item no formato "nome|quantidade"
+def receive_message(s):
+    data = s.recv(1024).decode()
+    parts = data.split('|')
+    return parts[0], parts[1]
 
-    # Associa o socket ao host e porta
-    server_socket.bind((host, port))
+def send_message(s, type, data):
+    data = f"{type}|{data}".encode()
+    s.sendall(data)
 
-    # Habilita o servidor para receber conexões
-    server_socket.listen(1)
-
-    print('Aguardando conexões...')
-
-    # Aceita a primeira conexão recebida
-    client_socket, addr = server_socket.accept()
-    print('Conexão estabelecida com:', addr)
-
-    while True:
-        # Recebe a mensagem do cliente
-        data = client_socket.recv(1024).decode()
-
-        if not data:
-            break
-
-        print('Solicitação recebida do cliente:', data)
-
-        # Processa a solicitação do cliente
-        response_data = process_request(data)
-
-        # Envia a resposta para o cliente
-        response_message = 'RESP ' + response_data
-        client_socket.send(response_message.encode())
-
-    # Fecha a conexão com o cliente
-    client_socket.close()
-    print('Conexão encerrada.')
-
-def process_request(request):
-    # Lógica para processar a solicitação do cliente
-    # e retornar a resposta adequada
-    # Aqui você implementaria a lógica específica
-    # do seu projeto integrador
-    response = 'Resposta do servidor'
-    return response
-
-if __name__ == '__main__':
-    start_server()
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    # Adicionar um item
+    send_message(s, 'ADD', 'Arroz|2')
+    # Receber a mensagem de resposta
+    type, data = receive_message(s)
+    if type == 'MSG':
+        print(f'Mensagem ecoada: {data}')
+    else:
+        print(f'Tipo de mensagem inválido: {type}')
+    # Remover um item
+    send_message(s, 'REMOVE', 'Arroz')
+    # Receber a mensagem de resposta
+    type, data = receive_message(s)
+    if type == 'MSG':
+        print(f'Mensagem ecoada: {data}')
+    else:
+        print(f'Tipo de mensagem inválido: {type}')
+    # Atualizar um item
+    send_message(s, 'UPDATE', 'Feijão|3')
+    # Receber a mensagem de resposta
+    type, data = receive_message(s)
+    if type == 'MSG':
+        print(f'Mensagem ecoada: {data}')
+    else:
+        print(f'Tipo de mensagem inválido: {type}')
+    # Buscar um item
+    send_message(s, 'GET', 'Feijão')
+    # Receber a mensagem de resposta
+    type, data = receive_message(s)
+    if type == 'MSG':
+        print(f'Mensagem ecoada: {data}')
+    else:
+        print(f'Tipo de mensagem inválido: {type}')
